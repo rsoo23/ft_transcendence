3f7cbe738d89f317f9197366c325163c90aacb2c
Ong Jing Hanz <hanzong111@LAPTOP-OSC5HOD8>
2024-08-14 16:01:55 +0800

    feat(Token_Management) : Refactored some codes , added verify_token() function

--- backend/token_management/views.py
+++ backend/token_management/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from main.settings import JWT_SECRET_KEY
+from django.contrib.auth import get_user_model
 
 @csrf_exempt
 def create_token(request):
@@ -11,12 +12,35 @@ def create_token(request):
         data = json.loads(request.body)
         username = data.get('username')
         password = data.get('password')
-        encoded_jwt = jwt.encode({"username": username, "password": password}, JWT_SECRET_KEY, algorithm= "HS256")
-        if encoded_jwt is not None:
-            response = HttpResponse("Set-Cookie")
-            response.set_cookie('ID_Token', encoded_jwt, httponly = 'True')
-            return response
-        else:
+        encoded_jwt = jwt.encode({'username': username, 'password': password}, JWT_SECRET_KEY, algorithm= 'HS256')
+        if encoded_jwt is None:
             return JsonResponse({'error': 'Token creation fail'}, status=405)
+        
+        response = HttpResponse('Sending back ID_Token cookie')
+        response.set_cookie('ID_Token', encoded_jwt, httponly = 'True', max_age=60)
+        return response
     return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+@csrf_exempt
+def verify_token(request) :
+    if request.method == 'GET':
+        if 'ID_Token' in request.COOKIES :
+            token = request.COOKIES['ID_Token']
+        else :
+            return JsonResponse({'success' : False, 'Error': 'ID_Token Not Found'}, status=401)
+        
+        if token == "":
+            return JsonResponse({'success' : False, 'Error' : 'Invalid token value'}, status=401)
+        decoded_jwt = jwt.decode(token, JWT_SECRET_KEY, algorithms="HS256")
+        User = get_user_model()
+        if User.objects.filter(username=decoded_jwt['username']).exists():
+            return JsonResponse({'success' : True, 'Status' : 'Token Bearer is Valid'})
+        else :
+            return JsonResponse({'success' : False, 'Status' : 'Token bearer not found'}, status=401)
+
+    return JsonResponse({'Error': 'Invalid request method'}, status=405)
+            
+        
+    
+    
     
