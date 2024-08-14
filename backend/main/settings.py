1b53c3f0ba677f33a26dff98b08c92569804b637
Ong Jing Hanz <hanzong111@LAPTOP-OSC5HOD8>
2024-08-14 13:30:42 +0800

    feat(Token_Management) : Moved secret string to settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-8(*2i*l0##=#g-=_d#%6tr!&i%20(ng%&9jiad!si-oa&ohex_'
+JWT_SECRET_KEY = 'applebananasgreenmonkey123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/token_management/views.py
+++ backend/token_management/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from main.settings import JWT_SECRET_KEY
 
 @csrf_exempt
 def create_token(request):
@@ -10,7 +11,7 @@ def create_token(request):
         data = json.loads(request.body)
         username = data.get('username')
         password = data.get('password')
-        encoded_jwt = jwt.encode({"username": username, "password": password}, "my-secret-key")
+        encoded_jwt = jwt.encode({"username": username, "password": password}, JWT_SECRET_KEY, algorithm= "HS256")
         if encoded_jwt is not None:
             response = HttpResponse("Set-Cookie")
             response.set_cookie('ID_Token', encoded_jwt, httponly = 'True')
