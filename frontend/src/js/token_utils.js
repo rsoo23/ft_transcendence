f94f80a24b41edd3d9945fe8d41342d1b84eeae8
rsoo23 <rongjie.soo12@gmail.com>
2024-08-31 11:14:29 +0800

    chore: delete login and menu js; move verifyToken to token_utils.js

---
+++ frontend/src/js/token_utils.js
@@ -0,0 +1,7 @@
+import { getRequest } from "./api_requests.js";
+
+async function verifyToken() {
+  const response = await getRequest('/token_management/verify_token')
+  const status = await response.json();
+  return (status.success);
+}
