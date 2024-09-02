9bfebbeea261c3ef9c0dbb0800f7b77763525a8f
rsoo23 <rongjie.soo12@gmail.com>
2024-09-02 12:59:09 +0800

    chore: move handle2FA to token_utils.js

--- frontend/src/js/network_utils/token_utils.js
+++ frontend/src/js/network_utils/token_utils.js
@@ -5,3 +5,27 @@ export async function verifyToken() {
   const status = await response.json();
   return (status.success);
 }
+
+export async function handle2FA() {
+  const email = document.getElementById('email').value;
+
+  try {
+    const response = await fetch('/two_factor_auth/enable_2FA/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email })
+    });
+
+    const data = await response.json();
+    if (data.success) {
+      alert('2FA Enabled using email:' + email);
+    } else {
+      alert('An error occurred. Please try again.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('An error occurred. Please try again.');
+  }
+}
