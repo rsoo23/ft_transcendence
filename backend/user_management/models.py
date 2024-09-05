86464241bb74232d4f96b707d64c728ff17d2cfe
Pei <nuyiep@hotmail.com>
2024-09-05 12:26:33 +0800

    fix: email cannot be registered twice

--- backend/user_management/models.py
+++ backend/user_management/models.py
@@ -6,4 +6,5 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 	two_factor_enabled = models.BooleanField(default=False)
 	two_factor_email = models.CharField(null=True, default="", max_length=30, blank=True)
+	email = models.EmailField(unique=True)
 	# pass # Not adding new fields
