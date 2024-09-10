f4656448cb577ed37a9fe1587e3537ff8ff2faf4
dastrukar <3derid@gmail.com>
2024-09-10 13:39:15 +0800

    test: update test files

--- frontend/src/js/websocket.js
+++ frontend/src/js/websocket.js
@@ -1,16 +1,32 @@
+import { postRequest } from './network_utils/api_requests.js';
+
 const INPUTS = {
 	38: 'moveUp',
 	40: 'moveDown',
 };
 var socket = null;
-var playerid = null;
+var matchID = null;
 var connected = false;
+
+async function createMatch() {
+	const player1_uuid = document.getElementById('player1').value;
+	const player2_uuid = document.getElementById('player2').value;
+	const response = await postRequest('/pong/create_match/', {
+		'player1_uuid': player1_uuid,
+		'player2_uuid': player2_uuid,
+	});
+	if (!response['success'])
+		return;
+
+	matchID = response['match_id'];
+	document.getElementById('matchid').textContent = `${matchID}`;
+}
 
 function connectToWebsocket() {
 	if (socket != undefined)
 		socket.close();
 
-	socket = new WebSocket('ws://localhost:8000/ws/pong');
+	socket = new WebSocket('ws://localhost:8000/ws/pong?test=true');
 	socket.addEventListener('message', (e) => {
 		console.log(e.data);
 	});
@@ -41,5 +57,9 @@ document.addEventListener(
 	() => {
 		document.addEventListener('keydown', keyUpHandler);
 		document.addEventListener('keyup', keyDownHandler);
+
+		document.getElementById('websocket').onclick = connectToWebsocket;
+		document.getElementById('testmatch').onclick = createMatch;
+		console.log("this is working yes");
 	}
 );
