8e2775d632d781e82b3b6b57b84791385587e96a
dastrukar <3derid@gmail.com>
2024-09-11 15:57:15 +0800

    fix(django): set CSRF_TRUSTED_ORIGINS to allow nginx csrf to work

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -42,6 +42,7 @@ if os.getenv('DJANGO_DEBUG', 'false').lower() in ['false', '']:
     DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000'] # TODO: change this to https when we have SSL
 
 
 # Application definition
