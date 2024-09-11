752fefd9d8612a79b11ce5842f6d10c7639edb8f
dastrukar <3derid@gmail.com>
2024-09-11 14:39:18 +0800

    test: update test files

--- frontend/src/js/websocket.js
+++ frontend/src/js/websocket.js
@@ -11,7 +11,7 @@ var connected = false;
 async function createMatch() {
 	const player1_uuid = document.getElementById('player1').value;
 	const player2_uuid = document.getElementById('player2').value;
-	const response = await postRequest('/pong/create_match/', {
+	const response = await postRequest('/pong/create-match/', {
 		'player1_uuid': player1_uuid,
 		'player2_uuid': player2_uuid,
 	});
@@ -19,14 +19,18 @@ async function createMatch() {
 		return;
 
 	matchID = response['match_id'];
-	document.getElementById('matchid').textContent = `${matchID}`;
+	document.getElementById('matchid').value = `${matchID}`;
 }
 
 function connectToWebsocket() {
 	if (socket != undefined)
 		socket.close();
 
-	socket = new WebSocket('ws://localhost:8000/ws/pong?test=true');
+	if (matchID == undefined) {
+		console.log('No match ID!');
+		return;
+	}
+	socket = new WebSocket(`ws://localhost:8000/ws/pong/${matchID}`);
 	socket.addEventListener('message', (e) => {
 		console.log(e.data);
 	});
@@ -60,6 +64,7 @@ document.addEventListener(
 
 		document.getElementById('websocket').onclick = connectToWebsocket;
 		document.getElementById('testmatch').onclick = createMatch;
+		document.getElementById('updatematchid').onclick = () => matchID = document.getElementById('matchid').value;
 		console.log("this is working yes");
 	}
 );
