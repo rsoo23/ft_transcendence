64da2448be158a6b339512d14ed4a836d99ed57b
Ong Jing Hanz <hanzong111@LAPTOP-OSC5HOD8>
2024-09-12 01:30:04 +0800

    feat(backend/two_factor_auth) : renamed enable_2FA view to email_2FA_send_code

--- backend/two_factor_auth/views.py
+++ backend/two_factor_auth/views.py
@@ -7,24 +7,9 @@ from main.settings import JWT_SECRET_KEY
 import jwt
 
 @csrf_exempt
-def enable_2FA(request):
+def email_2FA_send_code(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         email = data.get('email')
-        
-        if 'ID_Token' in request.COOKIES :
-            token = request.COOKIES['ID_Token']
-        else :
-            return JsonResponse({'success' : False, 'Error': 'ID_Token Not Found'}, status=401)
-        
-        if token == "":
-            return JsonResponse({'success' : False, 'Error' : 'Invalid token value'}, status=401)
-        
-        decoded_jwt = jwt.decode(token, JWT_SECRET_KEY, algorithms="HS256")
-        User = get_user_model()
-        user = User.objects.get(username=decoded_jwt['username'])
-        user.two_factor_email = email
-        user.two_factor_enabled = True
-        user.save()
-        return JsonResponse({'success' : True, 'Status' : '2FA Enabled'})
+        return JsonResponse({'success' : True, 'Status' : '2FA Token sent to :' + email})
     return JsonResponse({'error': 'Invalid request method'}, status=405)
