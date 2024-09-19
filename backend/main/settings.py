db3cf5f4805e54ecb43334a74270661de2719763
dastrukar <3derid@gmail.com>
2024-09-19 11:08:15 +0800

    feat(django): use sqlite3 when debug mode is on

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -117,16 +117,25 @@ else:
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': getenv_and_validate('DB_NAME'),
-        'USER': getenv_and_validate('DB_USER'),
-        'PASSWORD': getenv_and_validate('DB_PASSWORD'),
-        'HOST': getenv_and_validate('DB_HOST'),
-        'PORT': getenv_and_validate('DB_PORT'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': getenv_and_validate('DB_NAME'),
+            'USER': getenv_and_validate('DB_USER'),
+            'PASSWORD': getenv_and_validate('DB_PASSWORD'),
+            'HOST': getenv_and_validate('DB_HOST'),
+            'PORT': getenv_and_validate('DB_PORT'),
+        }
+    }
 
 
 # Password validation
