2b8fccd4028cab925cdc885c1120cc05b68d31ac
rsoo23 <rongjie.soo12@gmail.com>
2024-09-24 23:08:36 +0800

    feat(models): add Room and edit Message model

--- backend/realtime_chat/models.py
+++ backend/realtime_chat/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from user_management.models import CustomUser
+
+class Room(models.Model):
+    name = models.CharField(max_length=128)
+    online = models.ManyToManyField(to=CustomUser, blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
 
 class Message(models.Model):
-    text = models.TextField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class ChatRecord(models.Model):
