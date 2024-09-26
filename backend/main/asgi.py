b3ddafeed4e52c023ad05b88bb5a273466bba06e
rsoo23 <rongjie.soo12@gmail.com>
2024-09-26 09:45:58 +0800

    fix(asgi): fix DJANGO_SETTINGS_MODULE issue

--- backend/main/asgi.py
+++ backend/main/asgi.py
@@ -13,12 +13,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from realtime_chat.routing import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 django_asgi_app = get_asgi_application()
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatApp.settings")
-
-from realtime_chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
