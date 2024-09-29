1e5b84094d939233baa1069bd13a305a3280bdc3
rsoo23 <rongjie.soo12@gmail.com>
2024-09-29 18:54:49 +0800

    feat(admin): add admin config

--- backend/friends_system/admin.py
+++ backend/friends_system/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from friend.models import FriendRequest, FriendList
 
-# Register your models here.
+class FriendListAdmin(admin.ModelAdmin):
+    list_filter = ['user']
+    list_display = ['user']
+    search_fields = ['user']
+    readonly_fields = ['user']
+
+    class Meta:
+        model = FriendList
+
+admin.site.register(FriendList, FriendListAdmin)
