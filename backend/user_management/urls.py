fea1b820bf910bf95395e170144135dc26c24a9a
rsoo23 <rongjie.soo12@gmail.com>
2024-09-29 20:57:42 +0800

    feat(urls): add upload-avatar-image url

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	path('forgot_password/', views.forgot_password_view, name='forgot_password'),
 	path('update-username/', views.update_username, name='update_username'),
 	path('logout/', views.logout_view, name='logout'),
-	# path('hello/', views.hello_world, name='hello_world'),
 	path('update-password/', views.update_password, name='update_password'),
+    path('update-avatar-image/', views.update_avatar_image, name='update_avatar_image')
 ]
