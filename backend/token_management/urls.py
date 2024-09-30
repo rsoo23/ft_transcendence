3f52d0f88f15326f1316a5fdcee302be77be6d61
Ong Jing Hanz <hanzong111@LAPTOP-OSC5HOD8>
2024-10-01 01:22:02 +0800

    feat(backend/token_management : Added create_email_token() view - to issue email token if user with 'email' exists

--- backend/token_management/urls.py
+++ backend/token_management/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create_token/', views.create_token, name='create_token'),
     path('verify_token/', views.verify_token, name='verify_token'),
+    path('create_email_token/', views.create_email_token, name='create_email_token')
 ]
--- backend/token_management/views.py
+++ backend/token_management/views.py
@@ -40,3 +40,18 @@ def verify_token(request) :
             return JsonResponse({'success' : False, 'Status' : 'Token bearer not found'}, status=401)
 
     return JsonResponse({'Error': 'Invalid request method'}, status=405)
+
+@csrf_exempt
+def create_email_token(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        email = data.get('email')
+        encoded_jwt = jwt.encode({'email': email}, JWT_SECRET_KEY, algorithm= 'HS256')
+        if encoded_jwt is None:
+            return JsonResponse({'error': 'Token creation fail'}, status=405)
+        
+        response = JsonResponse({'success': True, 'message': 'Sending back Email_Token cookie'})
+        response.set_cookie('Email_Token', encoded_jwt, httponly = 'True', max_age=120)
+        return response
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
