6aa072284c6950ec20d15e65014f3071c1387f50
rsoo23 <rongjie.soo12@gmail.com>
2024-10-02 09:47:01 +0800

    feat(serializers): rearrage order of fields

--- backend/user_management/serializers.py
+++ backend/user_management/serializers.py
@@ -4,5 +4,20 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'  # Include all fields in the model
+        fields = [
+            'id',
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'date_joined',
+            'last_login',
+            'two_factor_enabled',
+            'two_factor_email',
+            'groups',
+            'user_permissions'
+        ]
 
