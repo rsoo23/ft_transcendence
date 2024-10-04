5085ae749a615d7f85fcf10b7903aa309d23dc16
rsoo23 <rongjie.soo12@gmail.com>
2024-10-04 10:55:09 +0800

    refactor(friends_system/admin): change user to current_user

--- backend/friends_system/admin.py
+++ backend/friends_system/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import FriendRequest, FriendList
 
 class FriendListAdmin(admin.ModelAdmin):
-    list_filter = ['user']
-    list_display = ['user']
-    search_fields = ['user']
-    readonly_fields = ['user']
+    list_filter = ['current_user']
+    list_display = ['current_user']
+    search_fields = ['current_user']
+    readonly_fields = ['current_user']
 
     class Meta:
         model = FriendList
