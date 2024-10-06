c2e348767d411241a2da498fc9b436fc1d7c2adc
rsoo23 <rongjie.soo12@gmail.com>
2024-10-06 22:02:01 +0800

    refactor(friends_system/views): refactor all views to function based views

--- backend/friends_system/urls.py
+++ backend/friends_system/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import SendFriendRequestView, GetSentFriendRequestsView, GetReceivedFriendRequestsView
+from .views import send_friend_request, get_sent_friend_requests, get_received_friend_requests
 
 urlpatterns = [
-    path('send_friend_request/', SendFriendRequestView.as_view(), name='send-friend-request'),
-    path('sent_friend_requests/', GetSentFriendRequestsView.as_view(), name='sent-friend-requests'),
-    path('received_friend_requests/', GetReceivedFriendRequestsView.as_view(), name='received-friend-requests'),
+    path('send_friend_request/', send_friend_request, name='send-friend-request'),
+    path('sent_friend_requests/', get_sent_friend_requests, name='sent-friend-requests'),
+    path('received_friend_requests/', get_received_friend_requests, name='received-friend-requests'),
 ]
--- backend/friends_system/views.py
+++ backend/friends_system/views.py
@@ -6,52 +6,48 @@ from .models import FriendList, FriendRequest
 
 from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
 
-class SendFriendRequestView(APIView):
-    permission_classes = [IsAuthenticated]
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def send_friend_request(request):
+    sender_id = request.user.id
+    receiver_id = request.data.get("receiver_id")
+    if not receiver_id:
+        return Response({"error": "Receiver user ID is required."}, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request):
-        sender_id = request.user.id
-        receiver_id = request.data.get("receiver_id")
-        if not receiver_id:
-            return Response({"error": "Receiver user ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        receiver = CustomUser.objects.get(pk=receiver_id)
+    except CustomUser.DoesNotExist:
+        return Response({"error": "Receiver user not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        try:
-            receiver = CustomUser.objects.get(pk=receiver_id)
-        except CustomUser.DoesNotExist:
-            return Response({"error": "Receiver user not found."}, status=status.HTTP_404_NOT_FOUND)
+    existing_request = FriendRequest.objects.filter(sender=request.user, receiver=receiver, is_active=True).first()
+    if existing_request:
+        return Response({"error": "An active friend request already exists."}, status=status.HTTP_400_BAD_REQUEST)
 
-        existing_request = FriendRequest.objects.filter(sender=request.user, receiver=receiver, is_active=True).first()
-        if existing_request:
-            return Response({"error": "An active friend request already exists."}, status=status.HTTP_400_BAD_REQUEST)
-
-        serializer = FriendRequestSerializer(data={
-            "sender": sender_id,
-            "receiver": receiver_id
-        })
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Friend request sent successfully."}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer = FriendRequestSerializer(data={
+        "sender": sender_id,
+        "receiver": receiver_id
+    })
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": "Friend request sent successfully."}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Get all friend requests where the current user is the sender
-class GetSentFriendRequestsView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        sent_requests = FriendRequest.objects.filter(sender=request.user, is_active=True)
-        serializer = FriendRequestSerializer(sent_requests, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_sent_friend_requests(request):
+    sent_requests = FriendRequest.objects.filter(sender=request.user, is_active=True)
+    serializer = FriendRequestSerializer(sent_requests, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 # Get all friend requests where the current user is the receiver
-class GetReceivedFriendRequestsView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        received_requests = FriendRequest.objects.filter(receiver=request.user, is_active=True)
-        serializer = FriendRequestSerializer(received_requests, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_received_friend_requests(request):
+    received_requests = FriendRequest.objects.filter(receiver=request.user, is_active=True)
+    serializer = FriendRequestSerializer(received_requests, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
