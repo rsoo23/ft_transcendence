1b8161423a9d561e7991bad69679d519526b9614
rsoo23 <rongjie.soo12@gmail.com>
2024-10-08 08:49:08 +0800

    feat(friends_system/views): add get_non_friends view

--- backend/friends_system/urls.py
+++ backend/friends_system/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import send_friend_request, get_sent_friend_requests, get_received_friend_requests
+from .views import send_friend_request, get_sent_friend_requests, get_received_friend_requests, get_non_friends
 
 urlpatterns = [
     path('send_friend_request/', send_friend_request, name='send-friend-request'),
     path('sent_friend_requests/', get_sent_friend_requests, name='sent-friend-requests'),
     path('received_friend_requests/', get_received_friend_requests, name='received-friend-requests'),
+    path('non_friends/', get_non_friends, name='non-friends'),
 ]
--- backend/friends_system/views.py
+++ backend/friends_system/views.py
@@ -1,6 +1,7 @@
 import json
 
 from user_management.models import CustomUser
+from user_management.serializers import CustomUserSerializer
 from .serializers import FriendRequestSerializer, FriendRequestCreateSerializer
 from .models import FriendList, FriendRequest
 
@@ -9,6 +10,43 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
+import logging
+logger = logging.getLogger(__name__)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_non_friends(request):
+    current_user = request.user
+
+    try:
+        friend_list = FriendList.objects.get(current_user=current_user)
+    except FriendList.DoesNotExist:
+        return Response({"error": "FriendList does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+    all_users = CustomUser.objects.all()
+
+    friends = friend_list.friends.all()
+
+    # gets the ids of the receivers of requests sent by current user
+    sent_requests = FriendRequest.objects.filter(sender=request.user, is_active=True)
+    requests_receivers = sent_requests.values_list('receiver__id', flat=True)
+    logger.info(requests_receivers)
+
+    # gets the ids of the request senders to current user
+    received_requests = FriendRequest.objects.filter(receiver=request.user, is_active=True)
+    requests_senders = received_requests.values_list('sender__id', flat=True)
+    logger.info(requests_senders)
+
+    # exclude:
+    # 1. all added friends
+    # 2. receivers of current user's friend request
+    # 3. senders of friend request to current user
+    # 4. current user
+    non_friends = all_users.exclude(id__in=friends).exclude(id__in=requests_receivers).exclude(id__in=requests_senders).exclude(id=current_user.id)
+
+    serializer = CustomUserSerializer(non_friends, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
