f985e59abdeda4c9833d3321dc1415f8f8624eb3
rsoo23 <rongjie.soo12@gmail.com>
2024-10-14 20:46:33 +0800

    feat(settings.py): add MEDIA_URL and MEDIA_ROOT

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -191,12 +191,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = './static'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'frontend', 'src'),
 ]
+
+# Media files images etc.
+MEDIA_URL = '/media/'  # URL to access media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Where uploaded media files will be stored
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
