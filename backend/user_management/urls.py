d58655b14b41a52905f2166bfe4c6ec2122510b2
rsoo23 <rongjie.soo12@gmail.com>
2024-10-16 14:53:46 +0800

    chore(user_management/urls): remove unused urls

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-	path('update-username/', views.update_username, name='update_username'),
-	path('forgot_password/', views.forgot_password_view, name='forgot_password'),
 	path('update_username/', views.update_username, name='update_username'),
 	path('logout/', views.logout_view, name='logout'),
  	path('email_exist/', views.email_exist, name='email_exist'),
