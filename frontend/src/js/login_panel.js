1115e982950c1b74580782e7b3759639f3c7383e
rsoo23 <rongjie.soo12@gmail.com>
2024-10-16 15:23:03 +0800

    chore(login_panel.js): remove unused code

--- frontend/src/js/login_panel.js
+++ frontend/src/js/login_panel.js
@@ -1,30 +1,10 @@
 
-import { getColor, getRandomColor } from "./ui_utils/color_utils.js";
+import { getColor } from "./ui_utils/color_utils.js";
 import { resetInputField, setInputFieldHint } from "./ui_utils/input_field_utils.js";
-import { loadMainMenuPanel } from "./main_menu_panel.js";
-import { postRequest, getRequest} from "./network_utils/api_requests.js";
-import { loadMainMenuContent, loadPage } from "./router.js";
+import { postRequest, getRequest } from "./network_utils/api_requests.js";
 // import { load2FAPanel } from "./2FA_panel.js";
 import { getIdToken } from "./network_utils/token_utils.js"
 // import { send_otp_2FA } from "./network_utils/2FA_utils.js";
-
-// export async function loadLoginPanel() {
-//   try {
-//     await loadComponent('components/login_panel.html')
-
-//     initBackButton(() => loadStartPanel())
-//     initRandomColorButton(
-//       'confirm-login-button',
-//       'login-panel',
-//       () => {
-//         handleLogin()
-//       }
-//     )
-//     initTogglePasswordVisibilityIcon()
-//   } catch (error) {
-//     console.error('Error loading Login Panel:', error)
-//   }
-// }
 
 export async function handleLogin() {
   const inputContainers = {
@@ -101,20 +81,3 @@ async function status_2FA() {
     return false
 }
 
-// export async function handleForgotPassword() {
-//   const email = document.getElementById('email').value;
-
-//   try {
-//     const response = await postRequest('/api/forgot-password/', { email })
-
-//     if (response.success) {
-//       alert('If an account exists with this email, password reset instructions have been sent.');
-//       location.reload(); // Reload to show login form
-//     } else {
-//       alert('An error occurred. Please try again.');
-//     }
-//   } catch (error) {
-//     console.error('Error:', error);
-//     alert('An error occurred. Please try again.');
-//   }
-// }
