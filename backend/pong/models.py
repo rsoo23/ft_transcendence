2720b9b97d135b2a4de4f7a51e33ae5360c33b83
dastrukar <3derid@gmail.com>
2024-10-21 03:44:58 +0800

    feat(pong): replace hosted with aborted

--- backend/pong/models.py
+++ backend/pong/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class PongMatch(models.Model):
-    hosted = models.BooleanField(default=False)
+    aborted = models.BooleanField(default=False)
     ended = models.BooleanField(default=False)
     player1_uuid = models.BigIntegerField()
     player2_uuid = models.BigIntegerField()
