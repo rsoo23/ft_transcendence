7d61598a48756dd776726211d8aa2f8b3b5446eb
dastrukar <3derid@gmail.com>
2024-11-03 00:30:00 +0800

    refactor(pong): move routing stuff to asgi.py

--- backend/main/asgi.py
+++ backend/main/asgi.py
@@ -14,14 +14,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
-from django.urls import path
+from django.urls import path, re_path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-from pong.routing import websocket_urlpatterns as pong_urlpatterns
 from realtime_chat.jwt_middleware import JWTAuthMiddleware
 from realtime_chat.consumers import ChatConsumer
 from friends_system.consumers import FriendsSystemConsumer
+from pong.consumers import PongConsumer
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
@@ -30,7 +30,8 @@ application = ProtocolTypeRouter({
             URLRouter([
                 path("ws/chat/<int:receiver_id>/", ChatConsumer.as_asgi()),
                 path("ws/friends_system/", FriendsSystemConsumer.as_asgi()),
-            ] + pong_urlpatterns)
+                re_path(r'ws/pong/(?P<match_id>[0-9]+)$', PongConsumer.as_asgi()),
+            ])
         )
     ),
 })
