009f463f3402d762f0715acfc1c4e36b2d9b0ca2
dastrukar <3derid@gmail.com>
2024-11-04 17:05:41 +0800

    chore: use redis for channels/cache while in debug mode
    also correctly uses redis hostname for cache

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -127,14 +127,20 @@ ASGI_APPLICATION = 'main.asgi.application'
 if DEBUG:
     CHANNEL_LAYERS = {
         'default': {
-            'BACKEND': 'channels.layers.InMemoryChannelLayer'
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {
+                'hosts': [('127.0.0.1', 6379)]
+            },
         }
-	}
+    }
     # Cache configuration
     CACHES = {
         'default': {
-            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-            'LOCATION': 'unique-snowflake',
+            'BACKEND': 'django_redis.cache.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379/1',
+            'OPTIONS': {
+                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            }
         }
     }
 
@@ -145,6 +151,16 @@ else:
             'CONFIG': {
                 'hosts': [('redis', 6379)]
             },
+        }
+    }
+
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_redis.cache.RedisCache',
+            'LOCATION': 'redis://redis:6379/1',
+            'OPTIONS': {
+                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            }
         }
     }
 
@@ -222,13 +238,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Where uploaded media files will 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user_management.CustomUser'
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
