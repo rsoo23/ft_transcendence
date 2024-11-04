7ca24ffb8cb7c6408aa149c421c45fe9f537a666
dastrukar <3derid@gmail.com>
2024-11-04 17:39:16 +0800

    fix(backend): define settings module before doing anything

--- backend/main/asgi.py
+++ backend/main/asgi.py
@@ -9,17 +9,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+
 from user_management.auth import JWTAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-
 from django.core.asgi import get_asgi_application
 from django.urls import path
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
 from pong.routing import websocket_urlpatterns as pong_urlpatterns
-from realtime_chat.jwt_middleware import JWTAuthMiddleware
 from realtime_chat.consumers import ChatConsumer
 from friends_system.consumers import FriendsSystemConsumer
 
