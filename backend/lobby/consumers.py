92015ea01a0e3a051ec7675a2822c169f2c3a2c9
dastrukar <3derid@gmail.com>
2024-11-04 14:36:33 +0800

    feat(lobby): do not broadcast lobby if full

--- backend/lobby/consumers.py
+++ backend/lobby/consumers.py
@@ -24,8 +24,18 @@ class LobbyConsumer(AsyncJsonWebsocketConsumer):
             return
 
         self.group_lobby = f'lobby-{self.lobby_id}'
+        self.max_users = model.max_users
         self.is_host = (self.user_id == model.host_id)
         if not self.is_host:
+            try:
+                users = json.loads(cache.get(self.group_lobby))
+                if len(users) >= self.max_users:
+                    await self.close(code=4002, reason='Lobby full')
+                    return
+
+            except Exception:
+                return
+
             await self.channel_layer.group_send(self.group_lobby, {
                 'type': 'lobby.notify.join',
                 'user': self.user_id,
@@ -105,6 +115,10 @@ class LobbyConsumer(AsyncJsonWebsocketConsumer):
         if not self.is_host:
             return
 
+        users = json.loads(cache.get(self.group_lobby))
+        if len(users) >= self.max_users:
+            return
+
         await self.channel_layer.send(event['channel'], {
             'type': 'lobby.receive.id',
             'id': self.lobby_id
@@ -119,6 +133,12 @@ class LobbyConsumer(AsyncJsonWebsocketConsumer):
     async def lobby_notify_left(self, event):
         if self.is_host:
             users = json.loads(cache.get(self.group_lobby))
+            if len(users) >= self.max_users:
+                await self.channel_layer.group_send(GROUP_LOBBYLIST, {
+                        'type': 'lobby.receive.id',
+                        'id': self.lobby_id,
+                    })
+
             for i in users:
                 if i['id'] != event['user']:
                     continue
@@ -137,6 +157,11 @@ class LobbyConsumer(AsyncJsonWebsocketConsumer):
             users = json.loads(cache.get(self.group_lobby))
             users.append({'id': event['user'], 'ready': False})
             cache.set(self.group_lobby, json.dumps(users))
+            if len(users) >= self.max_users:
+                await self.channel_layer.group_send(GROUP_LOBBYLIST, {
+                    'type': 'lobby.remove.id',
+                    'id': self.lobby_id,
+                })
 
         await self.send_json({
             'event': 'join',
