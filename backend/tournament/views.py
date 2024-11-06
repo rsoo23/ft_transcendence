eeb5bc886668f0c934eb5ff89ea2c25eba366dbf
dastrukar <3derid@gmail.com>
2024-11-07 04:53:12 +0800

    feat(tournament): refactor bracket and pair calculation

--- backend/tournament/views.py
+++ backend/tournament/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.core.cache import cache
 from .models import TournamentModel
 import json
+import math
 
 def create_tournament(lobby_id, host, users):
     tournament = TournamentModel.objects.create(host=host)
 
     user_amt = len(users)
-    pairs = int(user_amt / 2) + user_amt % 2
-    brackets = len(range(pairs)) - 1
+    pairs = math.ceil(user_amt / 2)
+    tmp_pairs = pairs
+    brackets = 0
+    while tmp_pairs > 0:
+        brackets += 1
+        tmp_pairs = math.ceil(tmp_pairs / 2)
+
     tournament_info = {
         'lobby_id': lobby_id,
         'pairs': pairs,
@@ -20,12 +26,15 @@ def create_tournament(lobby_id, host, users):
 
 def create_bracket(id, bracket_num, pairs, users):
     user_i = 0
+    pairs_arr = []
     for i in range(pairs):
         user1 = users[user_i]
         user2 = users[user_i + 1] if user_i + 1 < len(users) else None
         pair = (user1, user2)
+        pairs_arr.append(pair)
         user_i += 2
-        set_tournament_cache(id, f'tournament-{id}-{bracket_num}-{i}', json.dumps(pair))
+
+    set_tournament_cache(id, f'tournament-{id}-{bracket_num}', json.dumps(pairs_arr))
 
 # because there are too many to track
 def set_tournament_cache(id, key, value):
