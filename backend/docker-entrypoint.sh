f492a3fc85ad4fc2fe619034dd20b476f5b0b68f
dastrukar <3derid@gmail.com>
2024-11-08 14:29:48 +0800

    feat(docker): create superuser automatically, and add envs to do that

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env sh
 
+echo "====="
+echo "Collecting Django static files...."
+echo "-----"
 python manage.py collectstatic --no-input
+echo "====="
+echo "Migrating Django database changes...."
+echo "-----"
 python manage.py migrate
+echo "====="
+echo "Attempting to create superuser with .env values...."
+echo "-----"
+python manage.py createsuperuser --no-input
+echo "====="
+echo "Starting Django/Daphne...."
+echo "-----"
 daphne -p3000 -b0.0.0.0 "main.asgi:application"
