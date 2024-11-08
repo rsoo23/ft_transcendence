4e1e25a276883ea38a4d897b6c34a9eb6df15967
rsoo23 <rongjie.soo12@gmail.com>
2024-11-08 15:17:47 +0800

    fix(consumers.py): add self.scope[user] check

--- backend/realtime_chat/consumers.py
+++ backend/realtime_chat/consumers.py
@@ -14,32 +14,31 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.sender = self.scope['user']
 
-        if not self.sender.is_authenticated:
-            self.close()
-            return
+        if self.sender and self.sender.is_authenticated:
+            self.sender_id = self.sender.id
 
-        self.sender_id = self.sender.id
+            self.receiver_id = self.scope['url_route']['kwargs']['receiver_id']
+            self.receiver = get_object_or_404(CustomUser, pk=self.receiver_id)
 
-        self.receiver_id = self.scope['url_route']['kwargs']['receiver_id']
-        self.receiver = get_object_or_404(CustomUser, pk=self.receiver_id)
+            # create the room_name dynamically based on the sender's and receiver's ids
+            if self.sender_id < self.receiver_id:
+                self.room_name = f'chat_{self.sender_id}_{self.receiver_id}'
+            else:
+                self.room_name = f'chat_{self.receiver_id}_{self.sender_id}'
 
-        # create the room_name dynamically based on the sender's and receiver's ids
-        if self.sender_id < self.receiver_id:
-            self.room_name = f'chat_{self.sender_id}_{self.receiver_id}'
+            # get or create a room
+            self.room, created = Room.objects.get_or_create(name=self.room_name)
+
+            # get the sender and receiver and add them to the room
+            self.room.users.add(self.sender, self.receiver)
+
+            async_to_sync(self.channel_layer.group_add)(
+                self.room_name,
+                self.channel_name,
+            )
+            self.accept('Authorization')
         else:
-            self.room_name = f'chat_{self.receiver_id}_{self.sender_id}'
-
-        # get or create a room
-        self.room, created = Room.objects.get_or_create(name=self.room_name)
-
-        # get the sender and receiver and add them to the room
-        self.room.users.add(self.sender, self.receiver)
-
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_name,
-            self.channel_name,
-        )
-        self.accept('Authorization')
+            self.close()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
