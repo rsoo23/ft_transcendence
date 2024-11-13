e8205f08acfac4a4865045748dc2ef0e3cd5fc61
rsoo23 <rongjie.soo12@gmail.com>
2024-11-14 00:25:48 +0800

    feat: add how-to-play page 3

--- frontend/src/js/how_to_play.js
+++ frontend/src/js/how_to_play.js
@@ -5,14 +5,26 @@ export var howToPlayPageSwitcher = null
 export function initHowToPlayDivs() {
   const page1 = document.getElementById('how-to-play-page-1')
   const page2 = document.getElementById('how-to-play-page-2')
+  const page3 = document.getElementById('how-to-play-page-3')
+  // const page4 = document.getElementById('how-to-play-page-4')
+  // const page5 = document.getElementById('how-to-play-page-5')
 
-  howToPlayPageSwitcher = new PanelSwitcher(page1, [page1, page2])
-  document.getElementById('page-1-forward-button').onclick = () => howToPlayPageSwitcher.setCurrentDiv(
-    'how-to-play-page-1',
-    'how-to-play-page-2',
-  )
-  document.getElementById('page-2-back-button').onclick = () => howToPlayPageSwitcher.setCurrentDiv(
-    'how-to-play-page-2',
-    'how-to-play-page-1',
-  )
+  const pages = [page1, page2, page3]
+
+  howToPlayPageSwitcher = new PanelSwitcher(page1, pages)
+
+  pages.forEach((page, index) => {
+    if (index !== pages.length - 1) {
+      page.querySelector('.forward-button').onclick = () => howToPlayPageSwitcher.setCurrentDiv(
+        page.id,
+        pages[index + 1].id,
+      )
+    }
+    if (index !== 0) {
+      page.querySelector('.back-button').onclick = () => howToPlayPageSwitcher.setCurrentDiv(
+        page.id,
+        pages[index - 1].id,
+      )
+    }
+  })
 }
