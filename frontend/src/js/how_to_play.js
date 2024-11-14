15723d237c029adf86e30712304331aab2600368
rsoo23 <rongjie.soo12@gmail.com>
2024-11-14 09:43:55 +0800

    feat: align all forward and back buttons relative to content-container-outer and add logic functions initButton and toggleButtonVisibility

--- frontend/src/js/how_to_play.js
+++ frontend/src/js/how_to_play.js
@@ -1,6 +1,7 @@
 import { PanelSwitcher } from "./ui_utils/panelswitcher_utils.js"
 
 export var howToPlayPageSwitcher = null
+let currentPageIdx = 0
 
 export function initHowToPlayDivs() {
   const page1 = document.getElementById('how-to-play-page-1')
@@ -9,22 +10,57 @@ export function initHowToPlayDivs() {
   const page4 = document.getElementById('how-to-play-page-4')
   const page5 = document.getElementById('how-to-play-page-5')
 
+  const forwardButton = document.querySelector('.forward-button')
+  const backButton = document.querySelector('.back-button')
+
   const pages = [page1, page2, page3, page4, page5]
 
   howToPlayPageSwitcher = new PanelSwitcher(page1, pages)
 
-  pages.forEach((page, index) => {
-    if (index !== pages.length - 1) {
-      page.querySelector('.forward-button').onclick = () => howToPlayPageSwitcher.setCurrentDiv(
-        page.id,
-        pages[index + 1].id,
-      )
-    }
-    if (index !== 0) {
-      page.querySelector('.back-button').onclick = () => howToPlayPageSwitcher.setCurrentDiv(
-        page.id,
-        pages[index - 1].id,
-      )
-    }
-  })
+  initButtons(pages, forwardButton, backButton)
+  toggleButtonVisibility(pages.length, forwardButton, backButton)
 }
+
+// sets the new forward and back buttons divs when forward / back is clicked
+function initButtons(pages, forwardButton, backButton) {
+  if (currentPageIdx !== pages.length - 1) {
+    forwardButton.onclick = () => {
+      howToPlayPageSwitcher.setCurrentDiv(
+        pages[currentPageIdx].id,
+        pages[currentPageIdx + 1].id
+      )
+      currentPageIdx++
+      initButtons(pages, forwardButton, backButton)
+      toggleButtonVisibility(pages.length, forwardButton, backButton)
+    }
+  }
+
+  if (currentPageIdx !== 0) {
+    backButton.onclick = () => {
+      howToPlayPageSwitcher.setCurrentDiv(
+        pages[currentPageIdx].id,
+        pages[currentPageIdx - 1].id
+      )
+      currentPageIdx--
+      initButtons(pages, forwardButton, backButton)
+      toggleButtonVisibility(pages.length, forwardButton, backButton)
+    }
+  }
+}
+
+// if on the first page, hide the back button; otherwise show it
+// if on the last page, hide the forward button; otherwise show it
+function toggleButtonVisibility(pagesLen, forwardButton, backButton) {
+  if (currentPageIdx === 0) {
+    backButton.style.display = 'none'
+  } else {
+    backButton.style.display = ''
+  }
+
+  if (currentPageIdx === pagesLen - 1) {
+    forwardButton.style.display = 'none'
+  } else {
+    forwardButton.style.display = ''
+  }
+}
+
