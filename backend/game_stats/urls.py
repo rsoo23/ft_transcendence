e4f558890eaebb20e3df7ff32d121d65dfc1552a
rsoo23 <rongjie.soo12@gmail.com>
2024-11-19 10:26:14 +0800

    feat(backend): add pvp_tally and user_total_tally view

--- backend/game_stats/urls.py
+++ backend/game_stats/urls.py
@@ -7,4 +7,6 @@ router.register(r'match-stats', MatchStatsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('stats/pvp_tally/<int:user1_id>/<int:user2_id>/', UserVsUserStatsView.as_view(), name='pvp-tally'),
+    path('stats/user_total_tally/<int:user_id>/', UserTotalStatsView.as_view(), name='user-total-tally'),
 ]
